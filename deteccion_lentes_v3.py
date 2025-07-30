e7162892835c567a7fe58d15736edac49b516a92
Antho-321 <allunav@utn.edu.ec>
2025-07-30 17:26:07 -0500

    fix: comenta la línea de prueba para la detección de gafas en el script

--- deteccion_lentes_v3.py
+++ deteccion_lentes_v3.py
@@ -27,5 +27,6 @@ def glasses_detect(path):
 
 # 1) Put your image’s location in a variable
 #img_path = r"/kaggle/working/funcionalidades_validador_retratos/tests/test_image.jpg"
-img_path = r"tests/test_image.jpg"
-print(glasses_detect(img_path))      # → 'present' or 'absent'
+
+# img_path = r"tests/test_image.jpg"
+# print(glasses_detect(img_path))      # → 'present' or 'absent'
